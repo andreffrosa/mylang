94b6f06842fcb0efa19272332f59f4c5441dc317
Andre Rosa <25556799+andreffrosa@users.noreply.github.com>
2024-01-07 18:53:03 +0000

    Parse input files

--- src/cli/main.c
+++ src/cli/main.c
@@ -2,22 +2,57 @@
 
 #include "in/in.h"
 
-int main(int argc, char* argv[]) {
-    ParseContext ctx = inInitWithStdin();
+#define OPEN_FILE_ERR_MSG "Could not open the file %s\n"
+#define NO_FILE_ERR_MSG "No file provided!\n"
 
+int main(int argc, char *argv[]) {
     int result = 0;
     bool status = false;
-    do {
-        printf("> ");
 
-        ASTNode* ast = NULL;
-        status = inParse(ctx, &ast);
-        result = evalAST(ast);
+    if (argc == 1) {
+        ParseContext ctx = inInitWithStdin();
+        do {
+            printf("> ");
 
-        deleteASTNode(ast);
+            ASTNode *ast = NULL;
+            status = inParse(ctx, &ast);
+            result = evalAST(ast);
 
-        printf("= %d (%s)\n", result, status ? "OK" : "ERR");
-    } while (true);
+            deleteASTNode(ast);
 
-    inDelete(&ctx);
+            printf("= %d (%s)\n", result, status ? "OK" : "ERR");
+        } while (true);
+
+        inDelete(&ctx);
+    } else {
+        for (int i = 1; i < argc; i++) {
+            FILE *in_file = fopen(argv[i], "r");
+            if (in_file == NULL) {
+                fprintf(stderr, OPEN_FILE_ERR_MSG, argv[i]);
+                return 1;
+            }
+
+            ParseContext ctx = inInitWithFile(in_file);
+            ASTNode *ast = NULL;
+            status = inParse(ctx, &ast);
+
+            inDelete(&ctx);
+            fclose(in_file);
+
+            if (!status) {
+                fprintf(stderr, "Error parsing the file %s\n", argv[i]);
+                deleteASTNode(ast);
+                return 1;
+            }
+
+            printf("Parsed file %s: %d AST nodes.\n", argv[i], ast->size);
+
+            result = evalAST(ast);
+            printf("= %d (%s)\n", result, status ? "OK" : "ERR");
+
+            deleteASTNode(ast);
+        }
+    }
+
+    return 0;
 }
